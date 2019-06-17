c7fa67ec41a8e3d56dd64568d9110a02cb9ac12a
arrate7 <43437355+arrate7@users.noreply.github.com>
2019-06-17 17:04:29 +0200

    .

---
+++ RelacionNaN/RelacionNaN/Controllers/ExamenController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RelacionNaN.Models;
+
+namespace RelacionNaN.Controllers
+{
+    public class ExamenController : Controller
+    {
+        private readonly RelacionNaNContext _context;
+
+        public ExamenController(RelacionNaNContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Examen
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Examen.ToListAsync());
+        }
+       
+        // GET: Examen/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Examen examen = await _context.Examen
+                            .FindAsync(id);
+            if (examen == null)
+            {
+                return NotFound();
+            }
+
+            return View(examen);
+        }
+
+        // GET: Examen/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Examen/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Asignatura,Codigo")] Examen examen)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(examen);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(examen);
+        }
+
+        // GET: Examen/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Examen examen = await _context.Examen.FindAsync(id);
+            if (examen == null)
+            {
+                return NotFound();
+            }
+            return View(examen);
+        }
+
+        // POST: Examen/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Asignatura,Codigo")] Examen examen)
+        {
+            if (id != examen.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(examen);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExamenExists(examen.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(examen);
+        }
+
+        // GET: Examen/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Examen examen = await _context.Examen.FindAsync(id);
+            if (examen == null)
+            {
+                return NotFound();
+            }
+
+            return View(examen);
+        }
+
+        // POST: Examen/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var examen = await _context.Examen.FindAsync(id);
+            _context.Examen.Remove(examen);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Nota(int idExamen , int nota)
+        {
+            //simular Alumno id = 2
+            ExamenAlumno ea1 = new ExamenAlumno
+            {
+                Nota = nota,
+                Examen = await _context.Examen.FindAsync(idExamen),
+                Alumno = await _context.Alumno.FindAsync(2)
+            };
+
+            _context.Add(ea1);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+        private bool ExamenExists(int id)
+        {
+            return _context.Examen.Any(e => e.Id == id);
+        }
+    }
+}
